a0030bde37b27e67b4443bcfef80da1efaa916a1
Yuk Fang <yuk@YUKMBAIR.local>
2024-12-24 18:29:55 +0800

    rm bytefass

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Framework usage: https://bnpm.bytedance.net/package/@bytefaas/nodejs-framework-httpserver
-const { Logger } = require("@byted-service/logger");
-const logger = new Logger();
 const {koaApp, init} = require('./koaApp');
 const PORT = process.env.PORT || 80
 
